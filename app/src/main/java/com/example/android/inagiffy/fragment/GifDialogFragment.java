92bdb9d796529175a39738469dbc8c2a5bc706d4
Dan C <dcronin202@gmail.com>
2019-10-29 15:36:01 -0500

    Created a DialogFragment to show an enlarged view of the gif image when clicked

---
+++ app/src/main/java/com/example/android/inagiffy/fragment/GifDialogFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.inagiffy.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.android.inagiffy.R;
+
+
+public class GifDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_gif_image, container, false);
+
+        TextView textView = view.findViewById(R.id.dialog_title);
+        textView.setText("Images Coming Soon!");
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        return view;
+    }
+
+}

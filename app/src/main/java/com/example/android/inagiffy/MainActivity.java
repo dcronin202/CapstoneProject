4e762c7b607c4375fe63eb5e9b39660d2cd42c17
Dan C <dcronin202@gmail.com>
2019-10-29 08:44:23 -0500

    Added the MainActivityFragment to the main activity's onCreate

--- app/src/main/java/com/example/android/inagiffy/MainActivity.java
+++ app/src/main/java/com/example/android/inagiffy/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.inagiffy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.example.android.inagiffy.fragment.MainActivityFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivityFragment mainFragment = new MainActivityFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_activity_content, mainFragment)
+                .commit();
+
     }
+
 }

5f60327e6cf11cd838fec603bf90df6b636472d3
Dan C <dcronin202@gmail.com>
2019-10-30 13:49:22 -0500

    Added DataBinding

--- app/src/main/java/com/example/android/inagiffy/fragment/GifDialogFragment.java
+++ app/src/main/java/com/example/android/inagiffy/fragment/GifDialogFragment.java
@@ -4,32 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.android.inagiffy.R;
+import com.example.android.inagiffy.databinding.DialogGifImageBinding;
 
 
 public class GifDialogFragment extends DialogFragment {
 
+    private DialogGifImageBinding binding;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_gif_image, container, false);
 
-        TextView textView = view.findViewById(R.id.dialog_title);
-        textView.setText("Images Coming Soon!");
+        // DataBinding
+        binding = DataBindingUtil.inflate(inflater, R.layout.dialog_gif_image, container, false);
 
-        view.setOnClickListener(new View.OnClickListener() {
+        binding.dialogImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 getDialog().dismiss();
             }
         });
 
-        return view;
+        return binding.getRoot();
     }
 
 }

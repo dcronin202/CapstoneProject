5ceb19fc61c0e9b4bdb3ab98e8de646d7318793a
Dan C <dcronin202@gmail.com>
2019-10-30 07:50:39 -0500

    Changed file name

--- app/src/main/java/com/example/android/inagiffy/data/GiphyResponse.java
+++ app/src/main/java/com/example/android/inagiffy/data/GiphyResponse.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class TrendingResponse {
+public class GiphyResponse {
 
     @SerializedName("data")
     public List<Gif> trendingResults;

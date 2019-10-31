50e7eac4315465d26b14da3ea0003c8313085f22
Dan C <dcronin202@gmail.com>
2019-10-31 14:24:51 -0500

    Created a Menu for the MainActivity

--- app/src/main/java/com/example/android/inagiffy/MainActivity.java
+++ app/src/main/java/com/example/android/inagiffy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.android.inagiffy.fragment.MainActivityFragment;
 
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Menu Setup
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    // Method for Launching MainActivityFragment
     private void launchMainActivityFragment() {
         MainActivityFragment mainFragment = new MainActivityFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();

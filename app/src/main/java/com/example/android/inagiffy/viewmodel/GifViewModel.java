fe2be92ac10383054af976980a7baa6da9f94bee
Dan C <dcronin202@gmail.com>
2019-11-05 10:08:51 -0600

    Added methods for the view state and to add/remove favorites

--- app/src/main/java/com/example/android/inagiffy/viewmodel/GifViewModel.java
+++ app/src/main/java/com/example/android/inagiffy/viewmodel/GifViewModel.java
@@ -1,11 +1,14 @@
 package com.example.android.inagiffy.viewmodel;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 import com.example.android.inagiffy.data.Gif;
 
@@ -15,7 +18,12 @@ public class GifViewModel extends AndroidViewModel {
 
     private static final String LOG = GifViewModel.class.getSimpleName();
     private GifRepository gifRepository;
-    private Gif gifDetails;
+
+    private static final String viewState = "view_state";
+    public static final String trendingView = "trending";
+    public static final String favoritesView = "favorites";
+    private String currentView = "";
+    private SharedPreferences mPreferences;
 
 
     public GifViewModel(@NonNull Application application) {
@@ -28,30 +36,78 @@ public class GifViewModel extends AndroidViewModel {
         return gifRepository.getGifs();
     }
 
-    public Gif getGifDetails() {
-        return gifDetails;
-    }
-
-    public void setGifDetails(Gif gif) {
-        this.gifDetails = gif;
-    }
 
     // Favorites
-    public void addFavorite(Gif gif) {
-        gif.setIsFavorite(true);
+    public void getFavorites(LifecycleOwner owner) {
+        gifRepository.getFavoritesList(owner);
+        saveViewState(favoritesView);
+
     }
 
-    public void removeFavorite(Gif gif) {
+    public void addFavorite(final String gifId) {
+        gifRepository.getGifs().observeForever(new Observer<List<Gif>>() {
+            @Override
+            public void onChanged(List<Gif> gifList) {
+                for (int indexForNetworkResults = 0; indexForNetworkResults < gifList.size(); indexForNetworkResults++) {
+                    Gif gifFromNetwork = gifList.get(indexForNetworkResults);
+                    if (gifFromNetwork.getGifId().equals(gifId)) {
+                        gifFromNetwork.setIsFavorite(true);
+                        gifRepository.insertGif(gifFromNetwork);
+                    }
+                }
+                gifRepository.getGifs().removeObserver(this);
+            }
+        });
+    }
+
+    public void removeFavorite(final String gifId) {
+        gifRepository.getGifs().observeForever(new Observer<List<Gif>>() {
+            @Override
+            public void onChanged(List<Gif> gifList) {
+                for (int indexForNetworkResults = 0; indexForNetworkResults < gifList.size(); indexForNetworkResults++) {
+                    Gif gifFromNetwork = gifList.get(indexForNetworkResults);
+                    if (gifFromNetwork.getGifId().equals(gifId)) {
+                        gifFromNetwork.setIsFavorite(false);
+                        gifRepository.removeGif(gifFromNetwork);
+                    }
+                }
+                gifRepository.getGifs().removeObserver(this);
+            }
+        });
+    }
+
+    // View State
+    private void saveViewState(String newState) {
+        currentView = newState;
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putString(viewState, currentView);
+        editor.commit();
+    }
+
+    public void setupSharedPref(SharedPreferences sharedPreferences) {
+        if (mPreferences == null) {
+            this.mPreferences = sharedPreferences;
+        }
+    }
+
+    public void loadGifImages(LifecycleOwner owner) {
+        String showViewState = mPreferences.getString(viewState, "default");
+        if (showViewState.equals(favoritesView)) {
+            getFavorites(owner);
+        } else {
+            getTrendingGifList(owner);
+        }
     }
 
     // Trending
-    public void getTrendingGifList() {
-        gifRepository.callTrendingGifImages();
+    public void getTrendingGifList(LifecycleOwner owner) {
+        gifRepository.callTrendingGifImages(owner);
+        saveViewState(trendingView);
     }
 
     // Search
-    public void getSearchGifList(String query) {
-        gifRepository.callSearchGifImages(query);
+    public void getSearchGifList(String query, LifecycleOwner owner) {
+        gifRepository.callSearchGifImages(query, owner);
     }
 
 }
